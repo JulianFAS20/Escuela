1d18dbacc4d5d86330b60ef2519f8bb9b2ab2aba
Julian F Avila S <javila@lucasian.com>
2024-10-08 12:27:04 -0500

    Inicializacion del proyecto

---
+++ src/test/java/api/escula/EscuelaApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package api.escula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscuelaApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
